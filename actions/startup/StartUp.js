46938b18f1a19467bc4e9de017d53237312ec996
laynefaler <laynefaler@gmail.com>
2017-01-14 16:21:47 -0800

    changes

--- actions/startup/StartUp.js
+++ actions/startup/StartUp.js
@@ -1,7 +1,6 @@
 var TextCommand = require('./../commands/TextCommand.js');
 var PrintDocCommand = require('./../commands/PrintDocCommand.js');
 var CreateCommand = require('./../commands/CreateCommand.js');
-var PageCommand = require('./../commands/CreatePage.js');
 var ComponentCommand = require('./../commands/CreateComponent.js');
 var SecretCommand = require('./../commands/CreateSecret');
 
@@ -21,10 +20,7 @@ var StartUp = function(commandPattern, args, flags) {
         break;
       case 'make':
         commanderInCharge(ComponentCommand);
-        break;      
-      case 'page':
-        commanderInCharge(PageCommand);
-        break;      
+        break;          
       case 'secret':
         commanderInCharge(SecretCommand);
         break;
